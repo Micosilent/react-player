5d4b3cf6b2415792336582635133ad75396eaabf
Pau Costa Ferrer <mico@micodev.es>
2021-06-09 17:46:56 +0200

    added songs

---
+++ src/components/Player.js
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  //References
+  const audioRef = useRef(null);
+  //Event Handlers
+
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  return (
+    <div className="player-container">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          icon={faPlay}
+          size="2x"
+        />
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        />
+      </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
+    </div>
+  );
+};
+
+export default Player;
---
+++ src/util.js
@@ -0,0 +1,58 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Don't know why",
+      artist: "SwuM",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ccad9b1271d1a9701f84380bb217da7d923a6c4f-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7994",
+      color: ["#985B60", "#1B283B"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Stay",
+      artist: "SwuM",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ccad9b1271d1a9701f84380bb217da7d923a6c4f-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7998",
+      color: ["#985B60", "#1B283B"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Sodium",
+      artist: "Philanthrope, Tesk",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9149",
+      color: ["#69CCF6", "#F2E767"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Faces",
+      artist: "Knowmadic",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8554",
+      color: ["#69CCF6", "#F2E767"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Foggy Road",
+      artist: "Toonorth",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7834",
+      color: ["#69CCF6", "#F2E767"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
